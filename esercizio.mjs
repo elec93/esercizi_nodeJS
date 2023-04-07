cffab31c3dfd8926d1206ec5a17f0a482c6cc414
Eleonora <eleonoracasto93@gmail.com>
2023-04-07 13:21:24 +0200

    html resp

---
+++ esercizio.mjs
@@ -0,0 +1,18 @@
+// Our HTTP server sends an HTML response body.
+// Replace the text in the HTML with your own message. Run the server and use your web browser to test your changes.
+import { createServer } from "node:http";
+
+const server = createServer((request, response) => {
+  console.log("request received");
+
+  response.statusCode = 200;
+
+  response.setHeader("Content-Type", "text/html");
+
+  response.end(
+    "<html><body><h1 style='color: green'>Buona <span style='color: pink'>Pasqua!</span></h1></body></html>"  );
+});
+
+server.listen(3000, () => {
+  console.log(`Server running at http://localhost:3000`);
+});
