1868cfd0ce5f1880dfb8f1abc8e51c29051a0529
Eleonora <eleonoracasto93@gmail.com>
2023-04-13 16:07:13 +0200

    c

--- esercizio.mjs
+++ esercizio.mjs
@@ -1,11 +1,31 @@
-// Create a script that uses the Node.js core fs.writeFile() (callback API) method to write a text file. 
-//The documentation for this method is on the Node.js File system page.
-import { writeFile } from 'node:fs';
+// The `luckyDraw` function returns a promise. 
+// Create a promise chain where the function is called for for each of the players: Joe, Caroline and Sabrina
+// Log out the resolved value for each promise and handle any promise rejections in the chain.
+import * as fs from "node:fs/promises";
 
-writeFile('file.txt', 'Sono il testo del file', (error) => {
-  if (error){
-    console.log(error);
-    return
-  }
-  console.log('The file has been saved!');
+function luckyDraw(player) {
+  return new Promise((resolve, reject) => {
+    const win = Boolean(Math.round(Math.random()));
+
+    process.nextTick(() => {
+      if (win) {
+        resolve(`${player} won a prize in the draw!`);
+      } else {
+        reject(new Error(`${player} lost the draw.`));
+      }
+    });
+  });
+}
+
+luckyDraw(Joe) 
+.then((res) => console.log(res))
+luckyDraw(Caroline) 
+.then((res) => console.log(res))
+luckyDraw(Sabrina) 
+.then((res) => console.log(res))
+
+.catch((error) => {
+  console.error(error);
 });
+
+
