72638a0d20d2e6ba54dbfaa6aebe6b5a66d6d94f
Eleonora <eleonoracasto93@gmail.com>
2023-04-13 13:42:58 +0200

    writefile

--- esercizio.mjs
+++ esercizio.mjs
@@ -1,11 +1,11 @@
 // Create a script that uses the Node.js core fs.writeFile() (callback API) method to write a text file. 
 //The documentation for this method is on the Node.js File system page.
-
 import { writeFile } from 'node:fs';
-import { Buffer } from 'node:buffer';
 
-const data = new Uint8Array(Buffer.from('Hello Node.js'));
-writeFile('message.txt', data, (err) => {
-  if (err) throw err;
+writeFile('file.txt', 'Sono il testo del file', (error) => {
+  if (error){
+    console.log(error);
+    return
+  }
   console.log('The file has been saved!');
-}); 
+});
